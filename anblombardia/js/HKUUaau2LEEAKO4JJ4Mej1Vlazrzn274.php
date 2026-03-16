c093bd9df5831a0630cc3c7daaa5bc9d2d4f25bb
lucdemaio <93383556+lucdemaio@users.noreply.github.com>
2026-03-16 22:03:38 +0100

    Delete anblombardia directory

--- anblombardia/js/HKUUaau2LEEAKO4JJ4Mej1Vlazrzn274.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<aefer>".dirname(__FILE__)."</aefer>";
-?>
--- anblombardia/js/emailNoSpam.js
+++
@@ -1,7 +0,0 @@
-var user;
-var domain;
-var suffix;
-    
-function composeEmail(user, domain, suffix){
-  document.write('<a href="mailto:' + user + '@' + domain + '.' + suffix + '">' + user + '@' + domain + '.' + suffix + '</a>');
-}
